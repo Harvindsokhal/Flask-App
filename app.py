a870f10d0860d613f2cbeeed9b4a0de34652cc59
Harvindsokhal <harvindsokhal@gmail.com>
2021-06-28 16:56:54 +0100

    Table setup

--- app.py
+++ app.py
@@ -11,9 +11,11 @@ class Todo(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     content = db.Column(db.String(200), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
-    
 
-@app.route('/')
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+@app.route('/', methods=['POST', 'GET'])
 
 def index():
     return render_template('index.html')
