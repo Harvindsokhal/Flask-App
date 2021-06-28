3fb8c6ccb10f83f911719493d4417f672caf6092
Harvindsokhal <harvindsokhal@gmail.com>
2021-06-28 17:12:18 +0100

    Deleting Task

--- app.py
+++ app.py
@@ -18,7 +18,6 @@ class Todo(db.Model):
         return '<Task %r>' % self.id
 
 @app.route('/', methods=['POST', 'GET'])
-
 def index():
     if request.method == 'POST':
         task_content = request.form['content']
@@ -34,6 +33,19 @@ def index():
     else:
         tasks = Todo.query.order_by(Todo.date_created).all()
         return render_template('index.html', tasks=tasks)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There was a problem deleting that task'
+        
+
 
 
 if __name__ == '__main__':
